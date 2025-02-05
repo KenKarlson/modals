3d89ce12313c55a4c7383b97a4d9333e4c732fd4
Ken Karlson <javarecords@mail.ru>
2025-02-05 16:15:05 +0300

    New modal-validate-component

--- src/App.vue
+++ src/App.vue
@@ -3,49 +3,67 @@
     <div class="wrapper-content">
       <section>
         <div class="container">
-
           <!--first-modal-->
-          <button class="btn btnPrimary" @click="modalFirst = !modalFirst">First modal show</button>
-          <modals-component title="FirstModal" v-show="modalFirst" @close="modalFirst = false">
-          <div slot="bodyBox">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione dolor similique tempora cupiditate! Molestias ex earum suscipit aspernatur excepturi vel dignissimos, tempore beatae at, provident ipsa corporis mollitia laboriosam. Eveniet, alias nam earum eius provident id. Nesciunt accusamus dolores id eaque? Quos, nulla sequi. Culpa molestias tenetur sapiente voluptate magni id, non fugit voluptas facilis.</p>
-            <button class="btn btnSecondary" @click="modalFirst = !modalFirst">Well done</button>
-          </div>
+          <button class="btn btnPrimary" @click="modalFirst = !modalFirst">
+            First modal show
+          </button>
+          <modals-component
+            title="FirstModal"
+            v-show="modalFirst"
+            @close="modalFirst = false"
+          >
+            <div slot="bodyBox">
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Ratione dolor similique tempora cupiditate! Molestias ex earum
+                suscipit aspernatur excepturi vel dignissimos, tempore beatae
+                at, provident ipsa corporis mollitia laboriosam. Eveniet, alias
+                nam earum eius provident id. Nesciunt accusamus dolores id
+                eaque? Quos, nulla sequi. Culpa molestias tenetur sapiente
+                voluptate magni id, non fugit voluptas facilis.
+              </p>
+              <button
+                class="btn btnSecondary"
+                @click="modalFirst = !modalFirst"
+              >
+                Well done
+              </button>
+            </div>
           </modals-component>
 
           <!--second-modal-->
-          <button class="btn btnPrimary" @click="modalSecond.show = !modalSecond.show">Second modal show</button>
-          <modals-component
-          title="Second modal form"
-          v-show="modalSecond.show"
-          @close="modalSecond.show = false"
+          <button
+            class="btn btnPrimary"
+            @click="modalSecond.show = !modalSecond.show"
           >
-          <div slot="bodyBox">
-            <form action="" @submit.prevent="submitSecondForm">
-              <label for="">Name:</label>
-              <input type="text" v-model="modalSecond.name">
-              <label for="">Email:</label>
-              <input type="email" v-model="modalSecond.email">
-              <button class="btn btnSecondary" >Submit/Send</button>
-            </form>
-          </div>
+            Second modal show
+          </button>
+          <modals-component
+            title="Second modal form"
+            v-show="modalSecond.show"
+            @close="modalSecond.show = false"
+          >
+            <div slot="bodyBox">
+              <form action="" @submit.prevent="submitSecondForm">
+                <label for="">Name:</label>
+                <input type="text" v-model="modalSecond.name" />
+                <label for="">Email:</label>
+                <input type="email" v-model="modalSecond.email" />
+                <button class="btn btnSecondary">Submit/Send</button>
+              </form>
+            </div>
           </modals-component>
 
           <!--modal-validate-->
-          <button class="btn btnPrimary" @click="modalValidate.show = !modalValidate.show">Modal validate show</button>
-          <modal-validate-component title="Modal Validate" v-show="modalValidate.show" @close="modalValidate.show = false">
-            <div slot="bodyBox">
-            <form action="" @submit.prevent="submitValidateForm">
-              <label for="">Name:</label>
-              <input v-model="modalValidate.name" v-bind="modalValidate.name">
-              <label for="">Email:</label>
-              <input v-model="modalValidate.email" v-bind="modalValidate.email">
-              <button class="btn btnSecondary" >Submit/Send</button>
-            </form>
-          </div>
-
+          <button
+            class="btn btnPrimary"
+            @click="modalValidate = !modalValidate"
+          >
+            Modal validate show
+          </button>
+          <modal-validate-component v-show="modalValidate" @close="modalValidate = false">
+            
           </modal-validate-component>
-
         </div>
       </section>
     </div>
@@ -53,7 +71,7 @@
 </template>
 
 <script>
-import {required, minLenght, email} from 'vuelidate/lib/validators'
+import { required, minLenght, email } from "vuelidate/lib/validators";
 
 import ModalsComponent from "./components/ModalsComponent.vue";
 import ModalValidateComponent from "./components/ModalValidateComponent.vue";
@@ -65,45 +83,45 @@ export default {
     return {
       modalFirst: false,
       modalSecond: {
-        show:false,
-        name: '',
-        email: ''
+        show: false,
+        name: "",
+        email: "",
       },
-      modalValidate:{
-        show:false,
-        name: '',
-        email: '',
-      }
-    }
+      modalValidate: false,
+    };
   },
   validations: {
-    name:{}
+    name: {},
   },
-  methods:{
-    submitSecondForm(){
-      console.log(`Hello & welcome ! ${this.modalSecond.name}, ${this.modalSecond.email} `);
-      this.modalSecond.name = '';
-      this.modalSecond.email = '';
+  methods: {
+    submitSecondForm() {
+      console.log(
+        `Hello & welcome ! ${this.modalSecond.name}, ${this.modalSecond.email} `
+      );
+      this.modalSecond.name = "";
+      this.modalSecond.email = "";
       this.modalSecond.show = false;
     },
-    submitValidateForm(){
-      console.log(`Hello & welcome ! ${this.modalValidate.name}, ${this.modalValidate.email} `);
-      this.modalValidate.name = '';
-      this.modalValidate.email = '';
+    submitValidateForm() {
+      console.log(
+        `Hello & welcome ! ${this.modalValidate.name}, ${this.modalValidate.email} `
+      );
+      this.modalValidate.name = "";
+      this.modalValidate.email = "";
       this.modalValidate.show = false;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <style>
-.modal-enter{
+.modal-enter {
   opacity: 0;
 }
-.modal-leave-active{
+.modal-leave-active {
   opacity: 0;
 }
 .modal-enter .modal-content,
-.modal-leave-active .modal-content{
-  transform: scale(2.92s)
+.modal-leave-active .modal-content {
+  transform: scale(2.92s);
 }
 </style>
--- src/components/ModalValidateComponent.vue
+++ src/components/ModalValidateComponent.vue
@@ -1,95 +1,29 @@
 <template>
-  <modals-component
-  title="Modal title"
-  @close="$emit('close')">
-  <div slot="body">
-    <form @submit.prevent="submit">
-      <label>Name:</label>
-      <input v-bind="name">
-      <label>Email:</label>
-      <input v-bind="email">
-      <button class="btn btnSecondary" >Submit/Send</button>
-    </form>
-  </div>
-  </modals-component>
+<modals-component
+            title="Modal validate form comp"
+            @close="$emit('close')"
+          >
+          <button
+            class="btn btnPrimary"
+            @click="modalSecond.show = !modalSecond.show"
+          >
+            Second modal show
+          </button>
+            <div slot="bodyBox">
+              <form action="" @submit.prevent="">
+                <label for="">Name:</label>
+                <input type="text" />
+                <label for="">Email:</label>
+                <input type="email"  />
+                <button class="btn btnSecondary">Submit/Send</button>
+              </form>
+            </div>
+          </modals-component>
 </template>
 
 <script>
-import {required, minLenght, email} from 'vuelidate/lib/validators'
 import ModalsComponent from "./ModalsComponent.vue";
-
 export default {
-  components: { ModalsComponent },
-  data() {
-    return {
-      name:'',
-      email:''
-    };
-  },
-  valitations: {
-    name: {
-      required,
-      minLenght: minLenght(4)
-    },
-    email: {
-      required,
-      email
-    }
-  },
-}
+  components: { ModalsComponent},
+};
 </script>
-<style lang="scss" scoped>
-//modal animate
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-
-.modal__wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  transition: opacity 0.5s ease;
-  z-index: 998;
-  background-color: rgba(50, 70, 99, 0.3);
-}
-
-.modal-content {
-  position: relative;
-  max-width: 600px;
-  padding: 20px 18px;
-  background-color: #fff;
-  border: 1px solid #dcdfe6;
-  transition: all 0.3s ease;
-  border-radius: 8px;
-  z-index: 999;
-  overflow: hidden;
-  @media screen and (min-width: 900px) {
-    min-width: 500px;
-  }
-}
-.modal-header {
-  display: flex;
-  align-self: center;
-  justify-content: space-between;
-  padding-bottom: 20px;
-  span {
-    font-size: 24px;
-  }
-  .button-close {
-    cursor: pointer;
-  }
-}
-.modal-body {
-  text-align: center;
-}
-</style>
