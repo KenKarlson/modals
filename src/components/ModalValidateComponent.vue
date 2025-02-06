953a952c85da56086c24ce7e979d9fed81fef42d
Ken Karlson <javarecords@mail.ru>
2025-02-06 21:29:43 +0300

    add $v

--- src/components/ModalValidateComponent.vue
+++ src/components/ModalValidateComponent.vue
@@ -1,29 +1,49 @@
 <template>
-<modals-component
-            title="Modal validate form comp"
-            @close="$emit('close')"
-          >
-          <button
-            class="btn btnPrimary"
-            @click="modalSecond.show = !modalSecond.show"
-          >
-            Second modal show
-          </button>
-            <div slot="bodyBox">
-              <form action="" @submit.prevent="">
-                <label for="">Name:</label>
-                <input type="text" />
-                <label for="">Email:</label>
-                <input type="email"  />
-                <button class="btn btnSecondary">Submit/Send</button>
-              </form>
-            </div>
-          </modals-component>
+  <modals-component title="Modal validate form comp" @close="$emit('close')">
+    <button
+      class="btn btnPrimary"
+      @click="modalSecond.show = !modalSecond.show"
+    >
+      Second modal show
+    </button>
+    <div slot="bodyBox">
+      <form @submit.prevent="">
+        <div class="form-item" :class="{errorInput: $v.name.$error}">
+          <label for="">Name:</label>
+          <p class="errorText" v-if="!$v.name.$required">Name is required</p>
+          <p class="errorText" v-if="!$v.name.$minLenght">Name is lenghts </p>
+          <input v-bind="name" :class="{error: $v.name.$error}" @change="$v.name.$touch()" placeholder=""/>
+        </div>
+        
+        <label for="">Email:</label>
+        <input v-bind="email" />
+        <button class="btn btnSecondary">Submit/Send</button>
+      </form>
+    </div>
+  </modals-component>
 </template>
 
 <script>
+import { required, minLength, email } from "vuelidate/lib/validators";
+
 import ModalsComponent from "./ModalsComponent.vue";
 export default {
-  components: { ModalsComponent},
+  components: { ModalsComponent },
+  data() {
+    return {
+      name: "",
+      email: "",
+    };
+  },
+  validations: {
+    name: {
+      required,
+      minLength: minLength(4),
+    },
+    email: {
+      required,
+      email,
+    },
+  },
 };
 </script>
